7e9603a3572b050aa4ef403f2860b0e7402fb572
mtungij <jamesmtungi2013@gmail.com>
2025-01-12 12:07:49 +0300

    commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://eagledeyasmicrofinance.com/'; 
-// $config['base_url'] = 'http://localhost:8039/'; 
+// $config['base_url'] = 'https://eagledeyasmicrofinance.com/'; 
+$config['base_url'] = 'http://localhost:8039/'; 
 
 /*
 |--------------------------------------------------------------------------
